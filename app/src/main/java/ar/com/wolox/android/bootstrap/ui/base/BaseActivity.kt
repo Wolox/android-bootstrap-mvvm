7ad1c14f05b349b856993e32a596b8c7ee301842
ffasitella <federico.fasitella@wolox.com.ar>
2021-12-06 11:29:59 -0300

    Removed log

--- app/src/main/java/ar/com/wolox/android/bootstrap/ui/base/BaseActivity.kt
+++ app/src/main/java/ar/com/wolox/android/bootstrap/ui/base/BaseActivity.kt
@@ -1,6 +1,5 @@
 package ar.com.wolox.android.bootstrap.ui.base
 
-import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ProgressBar
@@ -19,14 +18,12 @@ open class BaseActivity : AppCompatActivity() {
 
     // TODO: Replace with View Binding
     fun showLoading() {
-        Log.e("asd", "asd")
         findViewById<ProgressBar>(R.id.loading).visibility = View.VISIBLE
         findViewById<FrameLayout>(R.id.fragmentContainer).visibility = View.GONE
     }
 
     // TODO: Replace with View Binding
     fun hideLoading() {
-        Log.e("asd", "asdo")
         findViewById<ProgressBar>(R.id.loading).visibility = View.GONE
         findViewById<FrameLayout>(R.id.fragmentContainer).visibility = View.VISIBLE
     }
