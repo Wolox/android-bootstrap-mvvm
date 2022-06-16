e1b0d63c8535ee583548fecd4835b75c21ca9eec
ffasitella <federico.fasitella@wolox.com.ar>
2022-06-16 12:13:17 -0300

    Added testing for SharedPreferencesManager

---
+++ app/src/test/java/ar/com/wolox/android/bootstrap/utils/UtilsTestsConstants.kt
@@ -0,0 +1,18 @@
+package ar.com.wolox.android.bootstrap.utils
+
+object UtilsTestsConstants {
+
+    const val MOCK_STRING = "String"
+    const val MOCK_INT = 123
+    const val MOCK_LONG = 1234L
+    const val MOCK_FLOAT = 1.2F
+    const val MOCK_BOOLEAN = true
+    const val KEY_STRING = "String"
+    const val KEY_INT = "Int"
+    const val KEY_LONG = "Long"
+    const val KEY_FLOAT = "Float"
+    const val KEY_BOOLEAN = "Boolean"
+    const val KEY_TO_CLEAN = "KeyToClean"
+    const val EXISTENT_KEY = "ExistingKey"
+    const val NON_EXISTENT_KEY = "NonExistentKey"
+}
